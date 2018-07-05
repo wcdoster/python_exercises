b10505b88a199530685a9fd9c1372021e829a6ac
Cole Doster <wcdoster@gmail.com>
2018-07-05 15:32:29 -0500

    dictionary exercise

---
+++ dictionaries/stocks.py
@@ -0,0 +1,26 @@
+stock_dict = {"GM": "General Motors", "CAT": "Caterpiller", "MSFT": "Microsoft", "AAPL": "Apple"}
+
+purchases = [("GM", 100, '5-july-2018', 25),
+("CAT", 30, "15-june-2018", 60),
+("MSFT", 60, "2-apr-2018", 30),
+("AAPL", 20, "4-may-2018", 70),
+("AAPL", 30, "5-june-2018", 65)]
+
+purchase_history = []
+
+for purchase in purchases:
+    item = (stock_dict[purchase[0]], purchase[1]*purchase[3])
+    purchase_history.append(item)
+
+stock_names = []
+purchase_total = {}
+
+for purchase in purchase_history:
+    if purchase[0] not in stock_names:
+        stock_names.append(purchase[0])
+        purchase_total[purchase[0]] = purchase[1]
+    else:
+        purchase_total[purchase[0]] = purchase_total[purchase[0]] + purchase[1]
+
+print(purchase_history)
+print(purchase_total)
